1c86a0c6ef5967db5e66d8354938a101d36abe35
subhash131 <subhashnayak131@gmail.com>
2023-11-06 23:54:23 +0530

    final touch

--- next.config.js
+++ next.config.js
@@ -9,6 +9,16 @@ const nextConfig = {
       },
     ];
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "www.okta.com",
+        port: "",
+        pathname: "/sites/default/files/Okta_Logo_White_Medium.png",
+      },
+    ],
+  },
 };
 
 module.exports = nextConfig;
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 
@@ -67,9 +68,9 @@ const Navbar = () => {
           <button
             data-tooltip-target="tooltip-new"
             type="button"
-            className="inline-flex items-center justify-center w-10 h-10 font-medium bg-blue-600 rounded-full hover:bg-blue-700 group focus:ring-4 focus:ring-blue-300 focus:outline-none dark:focus:ring-blue-800"
+            className="inline-flex items-center justify-center w-12 h-12 font-medium bg-blue-600 rounded-full hover:bg-blue-700 group focus:ring-4 focus:ring-blue-300 focus:outline-none dark:focus:ring-blue-800"
           >
-            <svg
+            {/* <svg
               className="w-4 h-4 text-white"
               aria-hidden="true"
               xmlns="http://www.w3.org/2000/svg"
@@ -83,8 +84,14 @@ const Navbar = () => {
                 strokeWidth="2"
                 d="M9 1v16M1 9h16"
               />
-            </svg>
-            <span className="sr-only">New item</span>
+            </svg> */}
+            {/* <span className="sr-only">New item</span> */}
+            <Image
+              src="https://www.okta.com/sites/default/files/Okta_Logo_White_Medium.png"
+              alt="logo"
+              width={90}
+              height={90}
+            />
           </button>
         </div>
         <div
@@ -130,7 +137,7 @@ const Navbar = () => {
           type="button"
           className="inline-flex flex-col items-center justify-center px-5 rounded-r-full hover:bg-gray-50 dark:hover:bg-gray-800 group"
         >
-          <svg
+          {/* <svg
             className="w-5 h-5 mb-1 text-gray-500 dark:text-gray-400 group-hover:text-blue-600 dark:group-hover:text-blue-500"
             aria-hidden="true"
             xmlns="http://www.w3.org/2000/svg"
@@ -138,8 +145,8 @@ const Navbar = () => {
             viewBox="0 0 20 20"
           >
             <path d="M10 0a10 10 0 1 0 10 10A10.011 10.011 0 0 0 10 0Zm0 5a3 3 0 1 1 0 6 3 3 0 0 1 0-6Zm0 13a8.949 8.949 0 0 1-4.951-1.488A3.987 3.987 0 0 1 9 13h2a3.987 3.987 0 0 1 3.951 3.512A8.949 8.949 0 0 1 10 18Z" />
-          </svg>
-          <span className="sr-only">Profile</span>
+          </svg> */}
+          <span className="text-white font-medium">Profile</span>
         </button>
         <div
           id="tooltip-profile"
